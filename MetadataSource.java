d8f5f0367d777bf1f98b410531867597763cd7e4
RXCE <120668809+Akhisapna@users.noreply.github.com>
2022-12-15 23:49:04 +0530

    Add files via upload

---
+++ MetadataSource.java
@@ -0,0 +1,13 @@
+package io.michaelrocks.libphonenumber.android;
+
+import io.michaelrocks.libphonenumber.android.Phonemetadata;
+
+interface MetadataSource {
+    Phonemetadata.PhoneMetadata getAlternateFormatsForCountry(int i);
+
+    Phonemetadata.PhoneMetadata getMetadataForNonGeographicalRegion(int i);
+
+    Phonemetadata.PhoneMetadata getMetadataForRegion(String str);
+
+    Phonemetadata.PhoneMetadata getShortNumberMetadataForRegion(String str);
+}
